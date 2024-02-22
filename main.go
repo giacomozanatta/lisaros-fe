e6da16c5dcc2cd9f7813b03d04cf435d30c8f361
Giacomo Zanatta <giacomo.zanatta@unive.it>
2024-02-22 14:12:36 +0100

    Fix
    Fix

--- main.go
+++ main.go
@@ -96,5 +96,5 @@ func main() {
 			"analysisStatus": string(bytes),
 		})
 	})
-	r.Run("lisaros-fe.onrender.com:9888") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":9888") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
